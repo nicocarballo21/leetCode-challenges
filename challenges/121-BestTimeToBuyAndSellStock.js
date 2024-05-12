0923314c98bc97a24dabbec4784a5166010de211
Nicolas carballo <nlucas120@gmail.com>
2024-05-12 02:12:51 -0300

    121-exercise done

--- challenges/121-BestTimeToBuyAndSellStock.js
+++ challenges/121-BestTimeToBuyAndSellStock.js
@@ -1,4 +1,4 @@
-export default function maxProfit (prices) {
+export default function maxProfit(prices) {
   let maxProfit = 0
   let minPrice = prices[0]
   for (let i = 1; i < prices.length; i++) {
